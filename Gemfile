688a912eb7dd989c47c3c8f0ef160a1ca1cb60bf
Filipe Crespo <filipe.crespo@locaweb.com.br>
2020-09-04 00:59:55 -0300

    Day two

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'jekyll'
+gem "jekyll"
+gem "jekyll-sass-converter"
+gem "jekyll-coffeescript"
 gem "jekyll-sitemap"
 gem "jekyll-paginate"
 gem "jekyll-seo-tag"
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task('sass', function () {
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(rename("style.css"))
         .pipe(gulp.dest('_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('assets/css'));
